501e8240e4f219f7bfbac17212f5c016881770f3
vaibhav-0310 <vbhargav0310@gmail.com>
2025-01-27 15:57:11 +0530

    backend1

---
+++ app.js
@@ -0,0 +1,52 @@
+//importing necessary files
+import express from "express";
+import mongoose from "mongoose";
+import ejsmate from "ejs-mate";
+import path from "path";
+import {dirname} from "path";
+import { fileURLToPath } from 'url';
+
+
+const app=express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//middlewares
+    app.engine("ejs",ejsmate);
+    app.set("view engine","ejs");
+    app.set("views",path.join(__dirname,"views"));
+    app.use(express.urlencoded({extended:true}))
+    app.use(express.static(path.join(__dirname,"/public")));
+
+    //connection to db
+    const createDB=async()=>{
+    await mongoose.connect("mongodb://127.0.0.1/SwapHub");
+};
+
+createDB()
+.then(()=>{
+    console.log("connected to db");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+//port listening
+app.listen(8080,()=>{
+    console.log("server started");
+});
+
+
+//home route
+app.get("/",(req,res)=>{
+    res.render("layouts/home.ejs");
+});
+
+//sell page
+app.get("/sell",(req,res)=>{
+     res.render("layouts/sell.ejs");
+});
+
+app.post("/sell",(req,res)=>{
+    
+})
